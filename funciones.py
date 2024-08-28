30b1c4372442a9422120501755e5585915fafecd
Javier Rojas <javierrojas14.jr@gmail.com>
2024-08-28 00:07:14 -0400

    Indica el numero de articulos distintos

--- funciones.py
+++ funciones.py
@@ -65,12 +65,16 @@ def obtener_resumen_de_articulos(string_articulos):
     resumen_articulos = pd.DataFrame(articulos_parseados).iloc[:-1]
 
     resumen_articulos["Authors"] = resumen_articulos["Authors"].apply(split_string)
+    resumen_articulos = resumen_articulos.reset_index(names="id_articulo")
     resumen_articulos = resumen_articulos.explode("Authors")
 
     return resumen_articulos
 
 
 def resumir_cantidad_de_autores(resumen_articulos):
+    # Obtiene el numero de articulos totales
+    n_articulos = max(resumen_articulos["id_articulo"]) + 1
+
     # Obtiene el conteo de autores con los que ha trabajado
     conteo_autores = resumen_articulos["Authors"].value_counts()
 
@@ -92,6 +96,7 @@ def resumir_cantidad_de_autores(resumen_articulos):
     # )
 
     return (
+        n_articulos,
         numero_autores_distintos,
         numero_autores_repetidos,
         numero_autores_que_ha_trabajado_una_vez,
